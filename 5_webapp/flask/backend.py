c0932e17fe744bd033982ac99f294547aed3b8ed
jimbauserman <jim.bauserman@gmail.com>
2019-08-02 14:02:19 -0400

    Added CORS policy for Flask

--- 5_webapp/flask/backend.py
+++ 5_webapp/flask/backend.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 import capstone_recommendation as rec
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods = ['GET','POST'])
 def root():
