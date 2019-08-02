524df9d97ca18df5a2f9f037dd9c0bdbbdc8288f
jimbauserman <jim.bauserman@gmail.com>
2019-08-02 12:27:30 -0400

    Changed IP again

--- 5_webapp/js/main.js
+++ 5_webapp/js/main.js
@@ -7,7 +7,7 @@ $(function() {
         figwidth = width - margin.left - margin.right,
         figheight = height - margin.top - margin.bottom;
 
-    var flask_ip = 'http://10.128.0.3:5000/'
+    var flask_ip = 'http://35.225.248.118:8000/'
 
     var query = d3.select("#query");
 
