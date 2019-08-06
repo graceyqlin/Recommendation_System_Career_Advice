6645d9de0028f45985ae76f0180cb30406ffc692
jimbauserman <jim.bauserman@gmail.com>
2019-08-06 18:20:15 -0400

    minor bug fix

--- 5_webapp/flask/clean_summary.py
+++ 5_webapp/flask/clean_summary.py
@@ -1,11 +1,11 @@
 from string import punctuation
 
-punc = punctuation.remove('-')
+punc = punctuation.replace('-','')
 
 def clean_sum(summary):
     sum_list = list(summary)
     for i in range(0,len(sum_list)):
-        if sum_list[i] in punctuation:
+        if sum_list[i] in punc:
             sum_list[i-1], sum_list[i]=sum_list[i], sum_list[i-1]
     for i in range(0,len(sum_list)):        
         if sum_list[i] == '.' and i != len(sum_list):
