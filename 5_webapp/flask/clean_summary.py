98635316ba51402ee96fee072701c1522b37cb21
jimbauserman <jim.bauserman@gmail.com>
2019-08-06 07:52:47 -0400

    Sentence casing for summaries

--- 5_webapp/flask/clean_summary.py
+++ 5_webapp/flask/clean_summary.py
@@ -5,4 +5,8 @@ def clean_sum(summary):
     for i in range(0,len(sum_list)):
         if sum_list[i] in punctuation:
             sum_list[i-1], sum_list[i]=sum_list[i], sum_list[i-1]
+        if sum_list[i] == '.' and i != len(sum_list):
+            sum_list[i+2] = sum_list[i+2].upper()
+    sum_list[0] = sum_list[0].upper()
+
     return ''.join(sum_list)
